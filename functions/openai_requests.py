e670bc06a9800b178a63bb2eb77a05bea1d07b4e
dibsnvas <d_beisenbayeva@kbtu.kz>
2024-07-26 17:56:05 +0500

    initial

--- functions/openai_requests.py
+++ functions/openai_requests.py
@@ -1,6 +1,9 @@
 import openai
 from functions.database import get_recent_messages
 
+# Make sure to set up your OpenAI API key
+openai.api_key = 'sk-proj-eecN1hd2PEneEI7OeNOIT3BlbkFJTVuG8hA9S0cP0IYcT7k1'
+
 def get_chat_response(message_input):
     messages = get_recent_messages()
     user_message = {"role": "user", "content": message_input}
@@ -11,7 +14,8 @@ def get_chat_response(message_input):
             model="gpt-3.5-turbo",
             messages=messages
         )
-        message_text = response["choices"][0]["message"]["content"]
+
+        message_text = response.choices[0].message['content']
         return message_text
     except Exception as e:
         print(f"Error in get_chat_response: {e}")
