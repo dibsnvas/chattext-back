e84bddf312a3054450b2257f5b8f319f5780eede
dibsnvas <d_beisenbayeva@kbtu.kz>
2024-07-26 18:31:32 +0500

    add

--- functions/openai_requests.py
+++ functions/openai_requests.py
@@ -8,7 +8,7 @@ def get_chat_response(message_input):
 
     try:
         def chat_gpt(prompt):
-            response = openai.ChatCompletion.create(
+            response = openai.chat.completions.create(
                 model="gpt-3.5-turbo",
                 messages=messages
             )
